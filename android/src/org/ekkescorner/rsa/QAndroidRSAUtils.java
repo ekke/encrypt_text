fe80fb1a346147f4b2956e9dbb4ce6b06d3bdb04
ekkes-corner <ekke@ekkes-corner.org>
2018-11-22 11:05:16 +0100

    alternate JAVA impl with android.util.Base64 is working

--- android/src/org/ekkescorner/rsa/QAndroidRSAUtils.java
+++ android/src/org/ekkescorner/rsa/QAndroidRSAUtils.java
@@ -4,18 +4,12 @@ package org.ekkescorner.rsa;
 
 import android.util.Log;
 import android.os.Build;
-import java.lang.String;
-import javax.crypto.Cipher;
-import java.security.spec.X509EncodedKeySpec;
-import java.security.PublicKey;
-import java.security.interfaces.RSAPublicKey;
+import android.util.Base64;
 import java.security.KeyFactory;
-import org.apache.commons.codec.binary.Base64;
+import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
 
-import java.security.GeneralSecurityException;
-import java.io.IOException;
-
-// import org.apache.commons.codec.binary.BaseNCodec;
+import javax.crypto.Cipher;
 
 
 public class QAndroidRSAUtils 
@@ -26,12 +20,13 @@ public class QAndroidRSAUtils
         //
     }
 
-    public static String PUBLIC_KEY = "-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDLuwt30JLYFvKcFOUdjPuDRdqv\nSnDb5TSdA/w0ND/GwLExpT66DeRz9+6//G//Y0y3c/yWT14k/ab1vID4U6W3vOgr\nafC0RyuIgH8ooCTNQpU+LtIoZ6qCejnux7VZ5lwWeT/9DQjWOtf6TopeRdzmOX09\nwa7c5xGGUsmi29QxDQIDAQAB\n-----END PUBLIC KEY-----";
-
+    // public static String PUBLIC_KEY = "-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDLuwt30JLYFvKcFOUdjPuDRdqv\nSnDb5TSdA/w0ND/GwLExpT66DeRz9+6//G//Y0y3c/yWT14k/ab1vID4U6W3vOgr\nafC0RyuIgH8ooCTNQpU+LtIoZ6qCejnux7VZ5lwWeT/9DQjWOtf6TopeRdzmOX09\nwa7c5xGGUsmi29QxDQIDAQAB\n-----END PUBLIC KEY-----";
+    public static String PUBLIC_KEY = "-----BEGIN PUBLIC KEY-----\nMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAgtaGkn1FfUmOb7nFK8DZ\nfU4JTtmuuLyJ+FRABhvvS+6rUQGiWyveJrLhMK4VI+zYBJrIKhKsDwQTbbZv/uV8\nW5ysCewUOsFt6clcZnc1IWAYOIw6D0B3TnodMwRBaR4kDsGRwMKy6S8T7WjhU6/0\nP8JLjtHQicO4pVqrnJiSJWsEXVuU4ClDRgok69wp7z/n6AHkllzGVgJMWM/JvQTn\ny30xrFxhlfDP3KjsLaiuUZwRxb7ZXDi9ovneulMwQyPSZi3Met0FpXfq5NfdsHqD\nPWZ1eSj8ghoOgbC9F1RMpgMz69tSF7lZYCgkNsl1N/k0E4Sw8uB6wAyLmgOeAn9G\n98/LdAHM7DFWJV2y18H7RXiUUtdtOSSvsp8PVbpnT8C/icHnXJbnvHZ81usUCo6x\nfUepmGEf5J3uA0+rmdMBOOlV/JfqjM04Ta+empRkyaOf3CF10Oc5Xm5fHUhWJ91v\nDS8G8EP/cKJe3wp1Ew2ItxbgDKTRAKq5dg83Mf3brgXppqTa4X6iNpUxm5fQmATi\nVk4+3/3x5k5rDky/iZQ1TtPPnQ4POiCvAfdq5i/cUkAnNKbeHadCB0wWzf7jsCXv\nF25mp+zNcgBUDcwLyAUXLzBVmxSSGLoLzqHSzln1hUUoRVZTvqU08g2eTfWknVY6\n7ZOsHxRHmBm0swykpTix5XcCAwEAAQ==\n-----END PUBLIC KEY-----";
     static String enccryptData(String txt)
     {
         Log.d("ekkescorner enccryptData", txt);
         String encodedString = "Encryption failed";
+        byte[] encrypted = null;
         try {
               String publicKeyPEM = PUBLIC_KEY;
               // Remove the first and last lines
@@ -39,25 +34,21 @@ public class QAndroidRSAUtils
               publicKeyPEM = publicKeyPEM.replace("-----END PUBLIC KEY-----", "");
               // Base64 decode data
               Log.d("ekkescorner enccryptData", publicKeyPEM);
-              byte[] encoded = Base64.decodeBase64(publicKeyPEM);
-              Log.d("ekkescorner enccryptData", "????");
-              KeyFactory kf = KeyFactory.getInstance("RSA");
-              RSAPublicKey pubKey = (RSAPublicKey) kf.generatePublic(new X509EncodedKeySpec(encoded));
-              Cipher cipher = Cipher.getInstance("RSA");
-              cipher.init(Cipher.ENCRYPT_MODE, pubKey);
-              encodedString = Base64.encodeBase64String(cipher.doFinal(txt.getBytes("UTF-8")));
-            }
-            catch (GeneralSecurityException e) {
-              e.printStackTrace();
-            }
-            catch (IOException e) {
-              e.printStackTrace();
-            }
-            catch (Exception e) {
-              e.printStackTrace();
-            }
-            finally {
-              return encodedString;
-            }
+              try {
+                  byte[] publicBytes = Base64.decode(publicKeyPEM, Base64.DEFAULT);
+                  X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicBytes);
+                  KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+                  PublicKey pubKey = keyFactory.generatePublic(keySpec);
+                  Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1PADDING"); //or try with "RSA"
+                  cipher.init(Cipher.ENCRYPT_MODE, pubKey);
+                  encrypted = cipher.doFinal(txt.getBytes());
+                  encodedString = Base64.encodeToString(encrypted, Base64.DEFAULT);
+              } catch (Exception e) {
+                  e.printStackTrace();
+              }
+              }
+              finally {
+                  return encodedString;
+              }
     }
 }
