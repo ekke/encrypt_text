ba7df276dce9f50a150c0acc18adffd11384afd1
ekkes-corner <ekke@ekkes-corner.org>
2018-11-22 11:06:25 +0100

    test key

--- android/src/org/ekkescorner/rsa/QAndroidRSAUtils.java
+++ android/src/org/ekkescorner/rsa/QAndroidRSAUtils.java
@@ -20,8 +20,7 @@ public class QAndroidRSAUtils
         //
     }
 
-    // public static String PUBLIC_KEY = "-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDLuwt30JLYFvKcFOUdjPuDRdqv\nSnDb5TSdA/w0ND/GwLExpT66DeRz9+6//G//Y0y3c/yWT14k/ab1vID4U6W3vOgr\nafC0RyuIgH8ooCTNQpU+LtIoZ6qCejnux7VZ5lwWeT/9DQjWOtf6TopeRdzmOX09\nwa7c5xGGUsmi29QxDQIDAQAB\n-----END PUBLIC KEY-----";
-    public static String PUBLIC_KEY = "-----BEGIN PUBLIC KEY-----\nMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAgtaGkn1FfUmOb7nFK8DZ\nfU4JTtmuuLyJ+FRABhvvS+6rUQGiWyveJrLhMK4VI+zYBJrIKhKsDwQTbbZv/uV8\nW5ysCewUOsFt6clcZnc1IWAYOIw6D0B3TnodMwRBaR4kDsGRwMKy6S8T7WjhU6/0\nP8JLjtHQicO4pVqrnJiSJWsEXVuU4ClDRgok69wp7z/n6AHkllzGVgJMWM/JvQTn\ny30xrFxhlfDP3KjsLaiuUZwRxb7ZXDi9ovneulMwQyPSZi3Met0FpXfq5NfdsHqD\nPWZ1eSj8ghoOgbC9F1RMpgMz69tSF7lZYCgkNsl1N/k0E4Sw8uB6wAyLmgOeAn9G\n98/LdAHM7DFWJV2y18H7RXiUUtdtOSSvsp8PVbpnT8C/icHnXJbnvHZ81usUCo6x\nfUepmGEf5J3uA0+rmdMBOOlV/JfqjM04Ta+empRkyaOf3CF10Oc5Xm5fHUhWJ91v\nDS8G8EP/cKJe3wp1Ew2ItxbgDKTRAKq5dg83Mf3brgXppqTa4X6iNpUxm5fQmATi\nVk4+3/3x5k5rDky/iZQ1TtPPnQ4POiCvAfdq5i/cUkAnNKbeHadCB0wWzf7jsCXv\nF25mp+zNcgBUDcwLyAUXLzBVmxSSGLoLzqHSzln1hUUoRVZTvqU08g2eTfWknVY6\n7ZOsHxRHmBm0swykpTix5XcCAwEAAQ==\n-----END PUBLIC KEY-----";
+    public static String PUBLIC_KEY = "-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDLuwt30JLYFvKcFOUdjPuDRdqv\nSnDb5TSdA/w0ND/GwLExpT66DeRz9+6//G//Y0y3c/yWT14k/ab1vID4U6W3vOgr\nafC0RyuIgH8ooCTNQpU+LtIoZ6qCejnux7VZ5lwWeT/9DQjWOtf6TopeRdzmOX09\nwa7c5xGGUsmi29QxDQIDAQAB\n-----END PUBLIC KEY-----";
     static String enccryptData(String txt)
     {
         Log.d("ekkescorner enccryptData", txt);
